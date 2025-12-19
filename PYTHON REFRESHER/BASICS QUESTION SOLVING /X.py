0f11579cacaf53cff3a21c74d82f1991a35dd748
MYMISERABLEMIND17 <tomarparakram17@gmail.com>
2025-12-19 20:35:34 +0530

    start learning numpy

---
+++ PYTHON REFRESHER/BASICS QUESTION SOLVING /X.py
@@ -0,0 +1,47 @@
+# x  = (1,5,2,4,5,5,7,)
+# i = 0
+# while (i<=len(x)-1):
+#     large = x[i]
+#     if(large < x[i]):
+#         large = x[i]
+#     i = i + 1
+# print(large)
+
+# -----------------------------------------------
+
+# tup = (3,5,2,5,3,6,1)
+# i = 0
+# even = 0
+# odd = 0
+#
+#
+# while( i < len(tup)):
+#     if(tup[i] %2 == 0 ):
+#         even = even +1
+#     else:
+#         odd = odd +1
+#     i = i+1
+#
+# print("total even " , even)
+# print("total odd " , odd)
+
+
+# --------------------------------------------------------------------
+
+# s = {10, 20, 30, 40}
+# for x in s:
+#     print(x)
+#
+#
+# a = {1, 2, 3}
+# b = {3, 4, 5}
+#
+# print(a | b)
+
+
+
+
+
+
+
+
